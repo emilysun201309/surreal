305065d9b41f95c1cfff186f1d58105e8ca0a082
xuetong <xuetong@umich.edu>
2019-01-31 14:55:22 -0500

    delete blender python and reinstall

--- setup.sh
+++ setup.sh
@@ -59,6 +59,12 @@ unzip church_outdoor_train_lmdb.zip
 
 cd ..
 
+#blender's default numpy is problematic
+#delete blender's numpy and reinstall
+rm -rf blender-2.78-linux-glibc219-x86_64/2.78/python/lib/python3.5/site-packages/numpy
+rm -rf blender-2.78-linux-glibc219-x86_64/2.78/python/lib/python3.5/site-packages/numpy-1.16.0.dist-info
+blender-2.78-linux-glibc219-x86_64/2.78/python/bin/python3.5m -m pip install numpy
+
 
 cd datageneration
 #change path in config file
