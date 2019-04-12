bbe217d198bcf374f7d21cb4c3d4b21ae0f8d867
xuetong <xuetong@umich.edu>
2019-04-12 01:17:49 -0400

    list video

--- video_prediction/listvideo.py
+++ video_prediction/listvideo.py
@@ -8,6 +8,6 @@ file_out = open("videolist.txt","w")
 # r=root, d=directories, f = files
 for r, d, f in os.walk(thisdir):
     for file in f:
-        if ".avi" in file:
+        if ".avi" in file and "m.avi" not in file and "d.avi" not in file:
             file_out.write(os.path.join(r,file) + '\n')
 file_out.close() 
