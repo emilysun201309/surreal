f02446d07bdf3797108c85e1886cdd4a3036aa68
xuetong <xuetong@umich.edu>
2019-04-12 01:07:34 -0400

    list video

---
+++ video_prediction/listvideo.py
@@ -0,0 +1,13 @@
+import os
+
+# Getting the current work directory (cwd)
+thisdir = os.getcwd()
+
+file_out = open("videolist.txt","w") 
+
+# r=root, d=directories, f = files
+for r, d, f in os.walk(thisdir):
+    for file in f:
+        if ".avi" in file:
+            file_out.write(os.path.join(r,file) + '\n')
+file_out.close() 
